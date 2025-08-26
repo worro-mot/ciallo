5d8980415cf9091753b93e5a1d2d7dd41bea28ab
BuildTools <unconfigured@null.spigotmc.org>
2025-08-25 22:41:50 -0700

    ghp

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ciallo/',
   server: {
     host: "::",
     port: 8080,
